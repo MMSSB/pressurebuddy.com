8f988c3c78d973590432e7ae3c785283a963f9a0
MMSSB <56227348+MMSSB@users.noreply.github.com>
2025-08-20 18:48:51 +0300

    Add files via upload

---
+++ loadlazy.js
@@ -0,0 +1,29 @@
+// const blurredImageDiv = document.querySelector(".blurred-img")
+// const img = blurredImageDiv.querySelector("img")
+// function loaded() {
+//   blurredImageDiv.classList.add("loaded")
+// }
+
+// if (img.complete) {
+//   loaded()
+// } else {
+//   img.addEventListener("load", loaded)
+// }
+
+
+
+    // // Initialize LazyLoad
+    // new LazyLoad({
+    //   elements_selector: ".blur-up",
+    //   callback_loaded: function(element) {
+    //     element.classList.add("lazyloaded");
+    //   }
+    // });
+
+
+    //     new LazyLoad({
+    //   elements_selector: ".lazy-img",
+    //   callback_loaded: function (img) {
+    //     img.classList.add("is-loaded"); // remove blur
+    //   }
+    // });
